93970e66d28325fc42918bf02a83e5039e12d541
Emon Ahmed <technicalboyemon@gmail.com>
2021-08-21 21:22:49 +0600

    Fixing 2

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-// Total Price Calculation Function - Function One
-function updateValue(value, type) {
-  document.getElementById(type + "-price").innerHTML = value;
-  const basic = document.getElementById("basic-price").innerHTML;
-  const memory = document.getElementById("memory-price").innerHTML;
-  const storage = document.getElementById("storage-price").innerHTML;
-  const delivery = document.getElementById("delivery-price").innerHTML;
-  const subtotal = document.getElementById("basic-subtotal");
-  subtotal.innerHTML = parseFloat(basic) + parseFloat(memory) + parseFloat(storage) + parseFloat(delivery);
-  document.getElementById("total").innerHTML = subtotal.innerHTML;
-}
-
-// Promocode Function For 20% Discount - Function Two
-document.getElementById("promo-code-click").addEventListener("click", function () {
-    const promo = document.getElementById("promo-code").value;
-    if (promo === "stevekaku") {
-      const basic_subtotal = document.getElementById("basic-subtotal").innerHTML;
-      const final_subtotal = parseFloat(basic_subtotal) - (parseFloat(basic_subtotal) / 100) * 20;
-      document.getElementById("total").innerHTML = final_subtotal;
-      document.getElementById("promo-code").value = "";
-    } else {
-      alert("Promo Code in not valid");
-    }
-  });
-
 // For Extra Memory Price
 document.getElementById("memory-8gb").addEventListener("click", function () {
   updateValue(0, "memory");
@@ -46,4 +21,34 @@ document.getElementById("prime-delivery").addEventListener("click", function () 
   });
 document.getElementById("fast-delivery").addEventListener("click", function () {
   updateValue(20, "delivery");
+});
+
+// Total Price Calculation Function - Function One
+function updateValue(value, type) {
+  document.getElementById(type + "-price").innerHTML = value;
+
+  const basic = document.getElementById("basic-price").innerHTML;
+  const memory = document.getElementById("memory-price").innerHTML;
+  const storage = document.getElementById("storage-price").innerHTML;
+  const delivery = document.getElementById("delivery-price").innerHTML;
+
+  const subtotal = document.getElementById("basic-subtotal");
+  subtotal.innerHTML = parseFloat(basic) + parseFloat(memory) + parseFloat(storage) + parseFloat(delivery);
+  document.getElementById("total").innerHTML = subtotal.innerHTML;
+}
+
+// Promocode Function For 20% Discount - Function Two
+function promoCode() {
+  const promo = document.getElementById("promo-code").value;
+  if (promo === "stevekaku") {
+    const basic_subtotal = document.getElementById("basic-subtotal").innerHTML;
+    const final_subtotal = parseFloat(basic_subtotal) - (parseFloat(basic_subtotal) * 0.20);
+    document.getElementById("total").innerHTML = final_subtotal;
+    document.getElementById("promo-code").value = "";
+  } else {
+    alert("Promo Code in not valid");
+  }
+}
+document.getElementById("promo-code-click").addEventListener("click", function () {
+  promoCode();
 });
