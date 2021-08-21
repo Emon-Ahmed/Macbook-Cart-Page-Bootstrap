d7182be4b83213aa9ee75f608d916ee303031079
Emon Ahmed <technicalboyemon@gmail.com>
2021-08-21 20:44:21 +0600

    Fixing

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
+// Total Price Calculation Function - Function One
 function updateValue(value, type) {
-  const compInput = document.getElementById(type + "-price");
-  compInput.innerHTML = value;
+  document.getElementById(type + "-price").innerHTML = value;
   const basic = document.getElementById("basic-price").innerHTML;
   const memory = document.getElementById("memory-price").innerHTML;
   const storage = document.getElementById("storage-price").innerHTML;
   const delivery = document.getElementById("delivery-price").innerHTML;
-  const total = document.getElementById("basic-subtotal");
-  total.innerHTML = parseFloat(basic) + parseFloat(memory) + parseFloat(storage) + parseFloat(delivery);
-  document.getElementById("total").innerHTML = total.innerHTML;
+  const subtotal = document.getElementById("basic-subtotal");
+  subtotal.innerHTML = parseFloat(basic) + parseFloat(memory) + parseFloat(storage) + parseFloat(delivery);
+  document.getElementById("total").innerHTML = subtotal.innerHTML;
 }
+
+// Promocode Function For 20% Discount - Function Two
 document.getElementById("promo-code-click").addEventListener("click", function () {
     const promo = document.getElementById("promo-code").value;
     if (promo === "stevekaku") {
-      const basic = document.getElementById("basic-subtotal").innerHTML;
-      const all = parseFloat(basic) - (parseFloat(basic) / 100) * 20;
-      document.getElementById("total").innerHTML = all;
+      const basic_subtotal = document.getElementById("basic-subtotal").innerHTML;
+      const final_subtotal = parseFloat(basic_subtotal) - (parseFloat(basic_subtotal) / 100) * 20;
+      document.getElementById("total").innerHTML = final_subtotal;
       document.getElementById("promo-code").value = "";
     } else {
       alert("Promo Code in not valid");
     }
   });
-// For Memory
+
+// For Extra Memory Price
 document.getElementById("memory-8gb").addEventListener("click", function () {
   updateValue(0, "memory");
 });
 document.getElementById("memory-16gb").addEventListener("click", function () {
   updateValue(180, "memory");
 });
-// For Storage
+// For Extra Storage Price
 document.getElementById("storage-256gb").addEventListener("click", function () {
   updateValue(0, "storage");
 });
@@ -37,7 +40,7 @@ document.getElementById("storage-512gb").addEventListener("click", function () {
 document.getElementById("storage-1tb").addEventListener("click", function () {
   updateValue(180, "storage");
 });
-// For  Delivery
+// For Extra Delivery Price
 document.getElementById("prime-delivery").addEventListener("click", function () {
     updateValue(0, "delivery");
   });
